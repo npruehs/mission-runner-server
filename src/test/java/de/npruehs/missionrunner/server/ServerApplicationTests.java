7beb8079194d8a18d548fb18359ec23958617dcb
Nick Prühs <dev@npruehs.de>
2019-12-18 10:05:58 +0100

    Add initial Spring Boot application.

---
+++ src/test/java/de/npruehs/missionrunner/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.npruehs.missionrunner.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
