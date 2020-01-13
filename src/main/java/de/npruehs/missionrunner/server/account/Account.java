b0764715822bd2a8c4a8af232fde56c0208a4cd4
Nick Prühs <dev@npruehs.de>
2020-01-13 19:51:19 +0100

    Generate random account names.

--- src/main/java/de/npruehs/missionrunner/server/account/Account.java
+++ src/main/java/de/npruehs/missionrunner/server/account/Account.java
@@ -2,11 +2,13 @@ package de.npruehs.missionrunner.server.account;
 
 public class Account {
 	private final String id;
+	private final String name;
 	private final int level;
 	private final int score;
 
-	public Account(String id, int level, int score) {
+	public Account(String id, String name, int level, int score) {
 		this.id = id;
+		this.name = name;
 		this.level = level;
 		this.score = score;
 	}
@@ -15,6 +17,10 @@ public class Account {
 		return id;
 	}
 
+	public String getName() {
+		return name;
+	}
+	
 	public int getLevel() {
 		return level;
 	}
--- src/main/java/de/npruehs/missionrunner/server/account/AccountController.java
+++ src/main/java/de/npruehs/missionrunner/server/account/AccountController.java
@@ -1,13 +1,25 @@
 package de.npruehs.missionrunner.server.account;
 
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+	private Random random;
+	
+	@PostConstruct
+	public void postConstruct() {
+		random = new Random();
+	}
+	
 	@RequestMapping("/account/get")
 	public Account get(@RequestParam(value = "id") String id) {
-		return new Account(id, 3, 1400);
+		String accountName = "Account" + random.nextInt(10000);
+		return new Account(id, accountName, 3, 1400);
 	}
 }
