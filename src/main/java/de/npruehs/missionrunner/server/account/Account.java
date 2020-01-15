59dfef906b106019611f6c8cb1903182ac0407c1
Nick Prühs <dev@npruehs.de>
2020-01-15 21:40:51 +0100

    Persist account data.

--- src/main/java/de/npruehs/missionrunner/server/account/Account.java
+++ src/main/java/de/npruehs/missionrunner/server/account/Account.java
@@ -1,11 +1,19 @@
 package de.npruehs.missionrunner.server.account;
 
-public class Account {
-	private final String id;
-	private final String name;
-	private final int level;
-	private final int score;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Account {
+	@Id
+	private String id;
+	private String name;
+	private int level;
+	private int score;
+
+	public Account() {
+	}
+	
 	public Account(String id, String name, int level, int score) {
 		this.id = id;
 		this.name = name;
@@ -17,15 +25,31 @@ public class Account {
 		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public int getLevel() {
 		return level;
 	}
 
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
 	public int getScore() {
 		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
---
+++ src/main/java/de/npruehs/missionrunner/server/account/AccountRepository.java
@@ -0,0 +1,6 @@
+package de.npruehs.missionrunner.server.account;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, String> {
+}
