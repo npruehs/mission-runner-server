e7c2e9623a42e3b38fecc9d7166f020747c67dbe
Nick Prühs <dev@npruehs.de>
2020-01-23 23:53:08 +0100

    Setup tutorial data.

--- src/main/java/de/npruehs/missionrunner/server/account/AccountController.java
+++ src/main/java/de/npruehs/missionrunner/server/account/AccountController.java
@@ -43,22 +43,18 @@ public class AccountController {
 		Account newAccount = new Account(id, accountName, 1, 0);
 		
 		// Create initial characters.
-		CharacterSkill[] skills = new CharacterSkill[2];
+		CharacterSkill[] skills = new CharacterSkill[1];
 		skills[0] = new CharacterSkill("A", 1);
-		skills[1] = new CharacterSkill("B", 1);
 		
-		Character[] characters = new Character[2];
-		characters[0] = new Character(newAccount, "TestCharacterA", skills);
-		characters[1] = new Character(newAccount, "TestCharacterB", skills);
-		
+		Character[] characters = new Character[1];
+		characters[0] = new Character(newAccount, "TutorialCharacter", skills);
+
 		// Create initial missions.
-		MissionRequirement[] requirements = new MissionRequirement[2];
-		requirements[0] = new MissionRequirement("A", 2);
-		requirements[1] = new MissionRequirement("B", 1);
+		MissionRequirement[] requirements = new MissionRequirement[1];
+		requirements[0] = new MissionRequirement("A", 1);
 		
-		Mission[] missions = new Mission[2];
-		missions[0] = new Mission(newAccount, "TestMissionA", requirements, 10, 100);
-		missions[1] = new Mission(newAccount, "TestMissionB", requirements, 10, 100);
+		Mission[] missions = new Mission[1];
+		missions[0] = new Mission(newAccount, "TutorialMission", requirements, 10, 100);
 		
 		// Save new account.
 		newAccount.setCharacters(Lists.newArrayList(characters));
