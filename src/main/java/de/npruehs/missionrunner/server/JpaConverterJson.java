bf327f939b269974df2d6141006aaccd56a9a5f0
Nick Prühs <dev@npruehs.de>
2020-04-19 13:45:00 +0200

    Add missing JSON JPA converter.

---
+++ src/main/java/de/npruehs/missionrunner/server/JpaConverterJson.java
@@ -0,0 +1,33 @@
+package de.npruehs.missionrunner.server;
+
+import java.io.IOException;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Converter(autoApply = true)
+public class JpaConverterJson implements AttributeConverter<Object, String> {
+
+	private final static ObjectMapper objectMapper = new ObjectMapper();
+
+	@Override
+	public String convertToDatabaseColumn(Object attribute) {
+		try {
+			return objectMapper.writeValueAsString(attribute);
+		} catch (JsonProcessingException ex) {
+			return null;
+		}
+	}
+
+	@Override
+	public Object convertToEntityAttribute(String dbData) {
+		try {
+			return objectMapper.readValue(dbData, Object.class);
+		} catch (IOException ex) {
+			return null;
+		}
+	}
+}
